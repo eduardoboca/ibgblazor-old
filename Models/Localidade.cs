dc495dc00fac5080ddc9521a3496d7ab8e11ca50
eduardoboca <eduardo.boca@gmail.com>
2023-12-11 16:04:05 -0300

    Equipe

---
+++ Models/Localidade.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IBGBlazor.Models
+{
+    public class Localidade
+    {
+        [Key]
+        [Column(TypeName = "char(7)")]
+        public string Id { get; set; }
+
+        [Column(TypeName = "char(2)")]
+        public string? State { get; set; }
+
+        [Column(TypeName = "nvarchar(80)")]
+        public string? City { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# ibgblazor
 Desafio .NET - Blazor - Balta.io
+
+Equipe 22
+Alexandre Souza
+Eduardo Freitas
+Renato Faria

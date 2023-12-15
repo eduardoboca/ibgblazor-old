667ff2b0a4dad0b5b30900942cb4a268c7973a6e
eduardoboca <eduardo.boca@gmail.com>
2023-12-15 15:40:54 -0300

    Authorize em todas as páginas

--- Program.cs
+++ Program.cs
@@ -27,7 +27,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
-builder.Services.AddQuickGridEntityFrameworkAdapter();;
+builder.Services.AddQuickGridEntityFrameworkAdapter(); ;
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)

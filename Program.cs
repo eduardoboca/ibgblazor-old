fa4c56b3cdd6ec7c494c8e27d026c5ea854df6ae
rfariadev <raf31283@gmail.com>
2023-12-15 11:59:55 -0300

    change connectionstring

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddAuthentication(options =>
     })
     .AddIdentityCookies();
 
-var connectionString = builder.Configuration.GetConnectionString("DevDefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
